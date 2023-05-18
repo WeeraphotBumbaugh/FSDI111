4cf2d69ae75415c8cbc860b3fdba9e3d44e87b61
Weeraphot Bumbaugh <weeraphot@bumbaugh.com>
2023-05-18 00:28:32 -0700

    updated routes, added sandbox

--- app/routes.py
+++ app/routes.py
@@ -1,12 +1,25 @@
-from flask import Flask                 # from the flask module import the Flask class
+from flask import Flask, request        # from the flask module import the Flask class
 
 app = Flask(__name__)                   # instantiate the Flask class into app (now an object)
 
-@app.get("/")                           # Flask decorator that maps a route to a view function. (@ = decorator) (.get = METHOD belongs to app)
+mylist = []
+
+@app.get("/aboutme")                           # Flask decorator that maps a route to a view function. (@ = decorator) (.get = METHOD belongs to app)
 def index():                            # a wrapped function is a view function in Flask
     me = {
         "first_name": "Weeraphot",
         "last_name": "Bumbaugh",
-        "hobbies": "polevaulting"
+        "hobbies": "polevaulting",
+        "is_active": True
     }
     return me                           # when you return a dictionary from a flask view function, flask will automatically convert it to JSON
+
+@app.post("/")
+def create_entry():
+    raw_data = request.json
+    mylist.append(raw_data)
+    return "", 204
+
+@app.get("/entries")
+def get_entries():
+    return mylist
